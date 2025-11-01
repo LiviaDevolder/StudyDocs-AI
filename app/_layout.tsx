5199aff44cae94ab36cf0aa3e7528b78e687c168
Lívia Devolder Pinheiro <liviadevolder@gmail.com>
2025-11-01 09:49:41 -0300

    feat: install native base

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,22 +3,33 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
+import { NativeBaseProvider, extendTheme } from 'native-base';
+
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
+const theme = extendTheme({
+  config: {
+    useSystemColorMode: true,
+  },
+});
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
+      <NativeBaseProvider theme={theme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        </Stack>
+        
+        <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+      </NativeBaseProvider>
     </ThemeProvider>
   );
 }
